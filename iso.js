04219a886d007d0e8b433bdbad96f755cd8712fa
Herman Rivas <hrivas5726@aol.com>
2020-03-18 22:56:56 -0400

    fixed syntax

--- iso.js
+++ iso.js
@@ -3,14 +3,11 @@ function iso(s, t) {
   let hash = {}
 
   for(let i=0; i < s.length; i++){
-    if(!hash[s[i]]){
+    if(!hash[s[i]]) {
       hash[s[i]] = t[i]
-    }
-
-    if(hash[s[i]] !== t[i]) {
+    } else if(hash[s[i]] !== t[i]) {
       return false
     }
-  }
   return true
 };
 
