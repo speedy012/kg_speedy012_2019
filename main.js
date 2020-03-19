364d56c4bc9209a1f287b982e6b7aee389f85298
Herman Rivas <hrivas5726@aol.com>
2020-03-18 22:48:08 -0400

    test1

---
+++ main.js
@@ -0,0 +1,16 @@
+function iso(s, t) {
+  if(s.length !== t.length) return false
+  let hash = {}
+
+  for(let i=0; i < s.length; i++){
+    if(!hash[s[i]]) {
+      hash[s[i]] = t[i]
+    }
+
+    if(hash[s[i]] !== t[i]) {
+      return false
+    }
+  return true
+};
+
+console.log(iso("bar", "foo"))
